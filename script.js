ca5fdb0b9cfc9f4c0c3c7b3110ffee1eac1678b5
Will J <138139664+WillJTools@users.noreply.github.com>
2023-07-10 13:28:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -105,8 +105,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function transformAnswer(answer) {
-        const specialCharacters = {'s': '$', 'i': '1', 'e': '3', 'o': '0'};
+  const specialCharacters = {'s': '$', 'i': '1', 'e': '3', 'o': '0', ' ': '_', 'a': '@'};
 
-        return Array.from(answer).map(char => specialCharacters[char.toLowerCase()] || char).join('');
+  let transformedAnswer = '';
+  let isCapitalized = false;
+
+  for (const char of answer) {
+    if (!isCapitalized && char.match(/[a-z]/i)) {
+      transformedAnswer += char.toUpperCase();
+      isCapitalized = true;
+    } else {
+      transformedAnswer += specialCharacters[char.toLowerCase()] || char;
     }
-});
+  }
+
+  return transformedAnswer;
+}
